c245ddb805dc832e0c4b73cc08266517b9d60c09
Kat Dobra <broun.kat@gmail.com>
2019-02-05 14:01:59 +0100

    Input component

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './Input.scss';
+
+const input = props => {
+  let inputElement = null;
+  const { elementConfig, value, id, change } = props;
+
+  switch (props.elementType) {
+    case 'input':
+      inputElement = (
+        <input
+          {...elementConfig}
+          value={value}
+          id={id}
+          onChange={change}
+          className="input-group__input"
+        />
+      );
+      break;
+    case 'textarea':
+      inputElement = (
+        <textarea
+          {...elementConfig}
+          value={value}
+          id={id}
+          onChange={change}
+          className="input-group__input"
+        />
+      );
+      break;
+    default:
+      inputElement = (
+        <input
+          {...elementConfig}
+          value={value}
+          id={id}
+          onChange={change}
+          className="input-group__input"
+        />
+      );
+  }
+
+  return (
+    <div className="input-group">
+      <label className="input-group__label" htmlFor={id}>
+        {props.label}
+      </label>
+      {inputElement}
+    </div>
+  );
+};
+
+export default input;
