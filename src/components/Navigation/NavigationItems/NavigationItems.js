082d3489deaadd9b7dd521b1f5d3cb00fe1ce23b
Kat Dobra <broun.kat@gmail.com>
2019-02-13 20:06:49 +0100

    small change in nav-list content

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,9 +5,9 @@ import './NavigationItems.scss';
 
 const navigationItems = props =>
   <ul className="main-nav">
-    <NavigationItem link="/">Заколочки</NavigationItem>
-    <NavigationItem link="/scrunchy">Резинки</NavigationItem>
-    <NavigationItem link="/afinka">Повязки</NavigationItem>
+    <NavigationItem link="/">Резинки и заколки</NavigationItem>
+    {/* <NavigationItem link="/scrunchy">Резинки</NavigationItem> */}
+    {/* <NavigationItem link="/afinka">Повязки</NavigationItem> */}
     <NavigationItem link="/delivery">Как заказать</NavigationItem>
     <NavigationItem link="/orders">Мои заказы</NavigationItem>
     <NavigationItem link="/impressum">Контакты</NavigationItem>
