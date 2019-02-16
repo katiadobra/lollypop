867fd3d7fa2a517289314a0105df49c87460c3a1
Kat Dobra <broun.kat@gmail.com>
2019-02-16 22:09:39 +0100

    Modal func-component to the Class

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Modal.scss';
 
-const modal = props =>
-  <div className="page-modal">
-    {props.children}
-  </div>;
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../Backdrop/Backdrop';
 
-export default modal;
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  componentWillUpdate() {
+    console.log('[Modal] WillUpdate');
+  }
+
+  render() {
+    const { show, modalClosed } = this.props;
+
+    return (
+      <Aux>
+        <Backdrop show={show} clicked={modalClosed} />
+        <div
+          className="page-modal"
+          style={{
+            transform: show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: show ? '1' : '0'
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
+
+export default Modal;
