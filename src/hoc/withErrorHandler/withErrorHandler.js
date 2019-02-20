e9bcc22563303851ef5945ca3e61069fc1b4e338
Kat Dobra <broun.kat@gmail.com>
2019-02-20 12:28:30 +0100

    fix: HOC withErrorHandler

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../Aux';
 
-const withErrorHandler = WrappedComponent => {
-  return props => {
-    return (
-      <Aux>
-        <Modal> Something didn't work!</Modal>
-        <WrappedComponent {...props} />
-      </Aux>
-    );
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+    state = {
+      error: null
+    };
+
+    componentWillMount() {
+      this.reqInterceptor = axios.interceptors.request.use(req => {
+        this.setState({ error: null });
+        return req;
+      });
+      this.resInterceptor = axios.interceptors.response.use(
+        res => res,
+        error => {
+          this.setState({ error: error });
+        }
+      );
+    }
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
+    }
+
+    errorConfirmedHandler = () => {
+      this.setState({ error: null });
+    };
+
+    render() {
+      const { error } = this.state;
+      return (
+        <Aux>
+          <Modal show={error} clicked={this.errorConfirmedHandler}>
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </Aux>
+      );
+    }
   };
 };
 
