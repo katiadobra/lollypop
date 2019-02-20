75c5c1fa8dff69ede6ce16732d8d6c4ad1c94387
Kat Dobra <broun.kat@gmail.com>
2019-02-20 10:11:39 +0100

    package: redux-thunk

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom';
 
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import itemActionsReducer from './store/reducers/itemActions';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   itemActionsReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 const app = (
