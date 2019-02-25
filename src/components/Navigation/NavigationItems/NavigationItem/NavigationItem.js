00ee1501d2c8b16769dcc0371d50eea90494346b
Kat Dobra <broun.kat@gmail.com>
2019-02-25 20:54:37 +0100

    feature: testing base: Jest and Enzyme

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const navigationItem = props =>
+  <li>
+    <NavLink to={props.link} exact={props.exact} activeClassName="active">
+      {props.children}
+    </NavLink>
+  </li>;
+
+export default navigationItem;
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 import './NavigationItems.scss';
+import NavigationItem from './NavigationItem/NavigationItem';
 
-const navigationItems = props =>
+const navigationItems = () =>
   <ul className="main-nav">
-    <NavigationItem link="/">Резинки и заколки</NavigationItem>
-    {/* <NavigationItem link="/scrunchy">Резинки</NavigationItem> */}
-    {/* <NavigationItem link="/afinka">Повязки</NavigationItem> */}
+    <NavigationItem link="/" exact>
+      Резинки и заколки
+    </NavigationItem>
     <NavigationItem link="/delivery">Как заказать</NavigationItem>
-    <NavigationItem link="/orders">Мои заказы</NavigationItem>
     <NavigationItem link="/impressum">Контакты</NavigationItem>
   </ul>;
-
-const NavigationItem = props =>
-  <li>
-    <NavLink to={props.link} exact>
-      {props.children}
-    </NavLink>
-  </li>;
 
 export default navigationItems;
