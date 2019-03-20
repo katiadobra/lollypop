8868f42d2e3574b0bccc0a4f7862b4c3a263467e
Kat Dobra <broun.kat@gmail.com>
2019-03-20 21:18:09 +0100

    fix: ThanksPage: return-button routing

--- src/containers/pages/ThanksPage/ThanksPage.js
+++ src/containers/pages/ThanksPage/ThanksPage.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './ThanksPage.scss';
 
-const thanksPage = props => {
-  return (
-    <div className="thank-you">
-      <section className="page-info">
-        <h1 className="thank-you__ttl">Спасибо за заказ!</h1>
-        <p>
-          Мы свяжемся с Вами по почте или по указанному номеру телефона для
-          уточнения деталей заказа и, после оплаты, доставим посылку в ближайшее
-          время.
-        </p>
-        <a className="btn btn--secondary" href="/">
-          Вернуться к покупкам
-        </a>
-      </section>
-    </div>
-  );
-};
+class ThanksPage extends Component {
+  returnHandler = () => {
+    this.props.history.push('/');
+  };
 
-export default thanksPage;
+  render() {
+    return (
+      <div className="thank-you">
+        <section className="page-info">
+          <h1 className="thank-you__ttl">Спасибо за заказ!</h1>
+          <p>
+            Мы свяжемся с Вами по почте или по указанному номеру телефона для
+            уточнения деталей заказа и, после оплаты, доставим посылку в
+            ближайшее время.
+          </p>
+          <button className="btn btn--secondary" onClick={this.returnHandler}>
+            Вернуться к покупкам
+          </button>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default ThanksPage;
